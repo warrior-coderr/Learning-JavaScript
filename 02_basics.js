3eceaefe950dc944d46578bae7aa16a0b5b4852e
Parikshit Yadav <ssparikshit03@gmail.com>
2025-05-17 04:05:01 +0000

    Functions Objects and Arrays

---
+++ 02_basics.js
@@ -0,0 +1,18 @@
+let myFunction = function(){
+    console.log("Parikshit")
+}
+
+myFunction()
+
+let myObj = {
+    name: "Roshita",
+    nickName: "Roshu",
+    age: 20,
+    love: "Parikshit"
+}
+
+console.log(myObj)
+
+const herAbilities = ["love", "scold", "cuteness", "beauty"]
+
+console.log(herAbilities)
