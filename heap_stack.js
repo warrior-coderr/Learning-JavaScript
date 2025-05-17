16ac6b30a427c750ea333dabd379f9924533d756
Parikshit Yadav <ssparikshit03@gmail.com>
2025-05-17 07:48:55 +0000

    Heap and Stack

---
+++ heap_stack.js
@@ -0,0 +1,14 @@
+let objOne = {
+    name : "Roshita",
+    age : 20
+}
+
+let objTwo = objOne
+
+console.log(objOne)
+console.log(objTwo)
+
+objTwo.name = "Roshu"
+
+console.log(objOne)
+console.log(objTwo)
